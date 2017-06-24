f112b409a875d1496e327c6f3bd7159830aacdcb
LSaldyt <lucassaldyt@gmail.com>
2017-06-24 14:01:44 -0600

    Adds new database, testing for

---
+++ clausedb.py
@@ -0,0 +1,43 @@
+from collections import defaultdict
+from clause      import Clause
+
+class ClauseDB(object):
+    def __init__(self):
+        self.dictionaries = []
+
+    def __str__(self):
+        return '\n'.join(str(node) for node in self.clauses())
+
+    def __contains__(self, other):
+        return len(self.get(other)) > 0
+
+    def clauses(self):
+        for clauseset in self.dictionaries[0].values():
+            for clause in clauseset:
+                yield clause
+
+    def add(self, node, clause):
+        for i, elem in enumerate(clause):
+            if len(self.dictionaries) < i + 1:
+                self.dictionaries.append(defaultdict(set))
+            self.dictionaries[i][elem].add(node)
+
+    def get(self, clause):
+        if clause == Clause('* * *'):
+            return [node.item for node in self.clauses()]
+        foundSets = []
+        for i, elem in enumerate(clause):
+            if i + 1 > len(self.dictionaries):
+                raise KeyError(
+                        'DataBase can only be indexed by tuples ' + \
+                        'equal or less in length to the tuples it stores')
+            if elem != '*':
+                foundSets.append(self.dictionaries[i][elem])
+        if len(foundSets) == 0:
+            return []
+        found = set.intersection(*foundSets)
+        return [node.item for node in found]
+
+
+
+
